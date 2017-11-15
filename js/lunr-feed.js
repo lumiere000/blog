270088278d55af8bf7d54893e6753a1c5509d5aa
lumiere000 <33013353+lumiere000@users.noreply.github.com>
2017-11-15 15:02:10 +0800

    Update lunr-feed.js

--- js/lunr-feed.js
+++ js/lunr-feed.js
@@ -38,12 +38,12 @@ console.log( jQuery.type(index) );
 
 var store = [{% for post in site.posts %}{
 //{% assign plink = post.url | prepend:'/blog'   %}
-//{% assign pimage = post.image | prepend:'/blog/posts/'   %}
+{% assign pimage = post.image | prepend:'/posts/'   %}
              
   "title": {{post.title | jsonify}},
 //  "link": {{ plink | jsonify}},
    "link": {{ post.url | jsonify}},
-//  "image": {{ pimage | jsonify }},
+  "image": {{ pimage | jsonify }},
   "image": {{ post.image | jsonify }},
   "date": {{ post.date | date: '%B %-d, %Y' | jsonify }},
   "category": {{ post.category | jsonify }},
